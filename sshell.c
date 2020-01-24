77cd4fb5733677726384766ba02d950f217f3054
Michael Ewing <maewing@ucdavis.edu>
2020-01-23 22:09:36 -0800

    Got pipeline to work independent of sshell program. Believe it to be an issue with how the parser handles the (char*)NULL added to the end of the processes. Also got the built-in commands working again and implimented the directory stack.

--- sshell.c
+++ sshell.c
@@ -2,7 +2,9 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/stat.h>
 #include <sys/wait.h>
+#include <stdbool.h> 
 
 #define CMDLINE_MAX 512
 #define ARG_MAX 17
@@ -12,30 +14,135 @@ typedef struct task Task;
 
 struct process {
 	char* args[ARG_MAX];
-	char* next_arg[ARG_MAX];
+	int pid;
 	char opp;
 	char opp_err;
-	int pid;
-	int fd[2];
-	int complete;
 };
 
 struct task {
 	Process processes[ARG_MAX];
+	int num_pros;
+	int num_pipes;
 };
 
-void parse_command(const char* cmd, Task* t) {
+struct StackNode {
+	char* nodeValue;
+	struct StackNode *nextNode;
+};
+
+struct StackNode *topPtr = (struct StackNode*)NULL;
+
+int CALL_CD(const char* path, const int retval) {
+	if (!strcmp(path, "..")) {
+		char* cwd = malloc(CMDLINE_MAX * sizeof(char));
+		getcwd(cwd, CMDLINE_MAX);
+		int slash_index;
+		for (int k = 0; k < ((int)strlen(cwd)) - 1; k++) {
+			if (cwd[k] == '/') {
+				slash_index = k;
+			}
+		}
+		char* newPath = malloc(CHAR_MAX * sizeof(char));
+		newPath = strncpy(newPath, cwd, slash_index);
+		chdir(newPath);
+	}
+	else {
+		if (chdir(path) < 0) {
+			fprintf(stderr, "Error: no such directory\n");
+			return(-1);
+		}
+	}
+	return(retval);
+}
+
+int CALL_PWD(const int retval) {
+	char buf[CMDLINE_MAX];
+	printf("%s\n", getcwd(buf, CMDLINE_MAX));
+	return(retval);
+}
+
+void CALL_EXIT() {
+	fprintf(stderr, "Bye...\n");
+	exit(0);
+}
+
+bool isEmpty() {
+	bool stackEmpty;
+	if (!topPtr) {
+		stackEmpty = true;
+	}
+	else {
+		stackEmpty = false;
+	}
+	return stackEmpty;
+}
+
+void PushStack(char* strings) {
+	struct StackNode *newNode = malloc(sizeof(struct StackNode));
+	newNode->nodeValue = malloc(CHAR_MAX * sizeof(char));
+	memcpy(newNode->nodeValue, strings, CHAR_MAX);
+	newNode->nextNode = topPtr;
+	topPtr = newNode;
+	if (isEmpty()) {
+		topPtr = newNode;
+		newNode->nextNode = NULL;
+	}
+	else {
+		newNode->nextNode = topPtr->nextNode;
+		topPtr = newNode;
+	}
+}
+
+void PopStack() {
+	struct StackNode *tempNode;
+	char* strings;
+	if (isEmpty()) {
+		fprintf(stderr, "Error: directory stack empty\n");
+	}
+	else {
+		strings = topPtr->nodeValue;
+		tempNode = topPtr->nextNode;
+		CALL_CD(strings, 0);
+		free(topPtr);
+		topPtr = tempNode;
+	}
+}
+
+void display() {
+	struct StackNode *pointer = NULL;
+	pointer = topPtr;
+	char buf[CMDLINE_MAX];
+	getcwd(buf, CMDLINE_MAX);
+	printf("%s\n", buf);
+	if (isEmpty())
+	{
+		fprintf(stderr, "\nError: directory stack empty");
+	}
+	while (pointer != NULL) {
+		printf("%s\n", pointer->nodeValue);
+		pointer = pointer->nextNode;
+	}
+}
+
+void PARSE_CMD(const char* cmd, Task* t) {
+	//create process to store the individual commands
 	Process p;
+	//create temp argument to store individual words
 	char temp_arg[CHAR_MAX];
 	memset(temp_arg, 0, CHAR_MAX);
+	//counts the number of processes in the Task
 	int process_counter = 0;
+	//counts number of arguements in each process
 	int arg_counter = 0;
+	//character counter so that the for-loop matches with the arguments
 	int char_counter = 0;
 	for (int i = 0; i < CMDLINE_MAX; i++) {
+		//check for spaces
 		if (cmd[i] == ' ') {
 			if (cmd[i - 1] == ' ' || char_counter == 0) {
 				continue;
 			}
+			//if space follows a word, store the word in p
 			else {
 				p.args[arg_counter] = malloc(CHAR_MAX * sizeof(char));
 				memcpy(p.args[arg_counter], temp_arg, CHAR_MAX);
@@ -45,39 +152,43 @@ void parse_command(const char* cmd, Task* t) {
 				continue;
 			}
 		}
-		else if (cmd[i] == '|' || cmd[i] == '>' || cmd[i] == '&') {
+		else if (cmd[i] == '|' || cmd[i] == '>') {
 			p.opp = cmd[i];
+			if (cmd[i] == '|') {
+				t->num_pipes += 1;
+			}
+			if (cmd[i + 1] == '&') {
+				p.opp_err = cmd[i + 1];
+			}
 			if (cmd[i - 1] != ' ') {
 				p.args[arg_counter] = malloc(CHAR_MAX * sizeof(char));
 				memcpy(p.args[arg_counter], temp_arg, CHAR_MAX);
+				memset(temp_arg, 0, CHAR_MAX);
+				arg_counter += 1;
+				char_counter = 0;
 			}
-			if (cmd[i] == '&') {
-				i += 1;
-				p.opp_err = cmd[i];
-			}
-			arg_counter += 1;
-			p.args[arg_counter] = malloc(CHAR_MAX * sizeof(char));
+			p.args[arg_counter] = malloc(CHAR_MAX * sizeof(char*));
 			p.args[arg_counter] = (char*)NULL;
-			memset(temp_arg, 0, CHAR_MAX);
 			t->processes[process_counter] = p;
+			t->num_pros += 1;
 			process_counter += 1;
 			arg_counter = 0;
-			char_counter = 0;
 			continue;
 		}
-		else if (cmd[i] == '\0') {
+		else if (cmd[i] == '\0' || cmd[i] == '\n') {
 			if (cmd[i - 1] != ' ') {
-				p.args[arg_counter] = malloc(CHAR_MAX * sizeof(char));
+				p.args[arg_counter] = malloc(CHAR_MAX * sizeof(char*));
 				memcpy(p.args[arg_counter], temp_arg, CHAR_MAX);
+				memset(temp_arg, 0, CHAR_MAX);
+				arg_counter += 1;
+				char_counter = 0;
 			}
-			arg_counter += 1;
-			p.args[arg_counter] = malloc(CHAR_MAX * sizeof(char));
+			p.args[arg_counter] = malloc(CHAR_MAX * sizeof(char*));
 			p.args[arg_counter] = (char*)NULL;
-			memset(temp_arg, 0, CHAR_MAX);
 			t->processes[process_counter] = p;
+			t->num_pros += 1;
 			process_counter += 1;
 			arg_counter = 0;
-			char_counter = 0;
 			break;
 		}
 		else {
@@ -87,112 +198,122 @@ void parse_command(const char* cmd, Task* t) {
 	}
 }
 
-
-int SYS_CALL(Task* t) {
+int PIPE_CMD(Task* t) {
 	pid_t _pid;
-	_pid = fork();
-	if (_pid == 0) {
-		execvp(t->processes[0].args[0], t->processes[0].args);
-		exit(-1);
-	}
-	else if (_pid > 0) {
-		int status;
-		waitpid(_pid, &status, 0);
-		return(status);
-	}
-	else {
-		perror("bad fork");
-		exit(-1);
+	int fd_in = (-1);
+	int fd_in_prev = (-1);
+	int fd[2];
+	for (int p = 0; p < t->num_pros; p++) {
+		pipe(fd);
+		_pid = fork();
+		if (_pid > 0) {
+			// Parent
+			if (t->processes[p].opp == '|' && fd_in_prev != -1) {
+				fd_in = fd[0];
+				close(fd[1]);
+			}
+			else if (t->processes[p].opp == '|') {
+				fd_in = fd[0];
+				close(fd[1]);
+			}
+			else if (fd_in_prev != -1) {
+				fd_in = (-1);
+				close(fd[0]);
+				close(fd[1]);
+			}
+			fd_in_prev = fd_in;
+			fd_in = (-1);
+			int status;
+			waitpid(_pid, &status, 0);
+		}
+		else if (_pid == 0) {
+			// Child
+			if (t->processes[p].opp == '|' && fd_in_prev != -1) {
+				dup2(fd_in_prev, STDIN_FILENO);
+				dup2(fd[1], STDOUT_FILENO);
+				close(fd[0]);
+				close(fd[1]);
+			}
+			else if (t->processes[p].opp == '|') {
+				dup2(fd[1], STDOUT_FILENO);
+				close(fd[0]);
+				close(fd[1]);
+			}
+			else if (fd_in_prev != -1) {
+				dup2(fd_in_prev, STDIN_FILENO);
+				close(fd[0]);
+				close(fd[1]);
+			}
+			execvp(t->processes[p].args[0], t->processes[p].args);
+			perror("bad exec");
+			return(-1);
+		}
+		else {
+			perror("bad fork");
+			return(-1);
+		}
 	}
 	return 0;
 }
 
-
-int main(void)
-{
+int main(void) {
 	char cmd[CMDLINE_MAX];
-
 	while (1) {
 		Task Run;
+		Run.num_pipes = 0;
+		Run.num_pros = 0;
 		char *nl;
 		int retval = 0;
-
 		/* Print prompt */
 		printf("sshell$ ");
 		fflush(stdout);
-
 		/* Get command line */
 		fgets(cmd, CMDLINE_MAX, stdin);
-
 		/* Print command line if stdin is not provided by terminal */
 		if (!isatty(STDIN_FILENO)) {
 			printf("%s", cmd);
 			fflush(stdout);
 		}
-
 		/* Remove trailing newline from command line */
 		nl = strchr(cmd, '\n');
 		if (nl) {
 			*nl = '\0';
 		}
-
-		parse_command(cmd, &Run);
-
-		/*
-			for(int m=0;m<3;m++){
-			  printf("Process %d:\n", m);
-			  for(int n=0;n<5;n++){
-				  printf("%s\n", Run.processes[m].args[n]);
-			  }
-			}
-		*/
-
-		/* Builtin command */
+		PARSE_CMD(cmd, &Run);
+		char* cmd = Run.processes[0].args[0];
 		if (!strcmp(cmd, "exit")) {
-			fprintf(stderr, "Bye...\n");
-			fprintf(stdout, "+ completed '%s' [%d]\n", cmd, retval);
-			break;
-		}
-
-		if (!strcmp(cmd, "pwd")) {
-			char buf[CMDLINE_MAX];
-			printf("%s\n", getcwd(buf, CMDLINE_MAX));
+			CALL_EXIT();
 			fprintf(stdout, "+ completed '%s' [0]\n", cmd);
-			continue;
 		}
-
-		if (!strcmp(Run.processes[0].args[0], "cd")) {
-			char* path = Run.processes[0].args[2];
-			if (!strcmp(path, "..")) {
-				char* cwd = getcwd(cwd, CMDLINE_MAX);
-				int slash_index;
-				for (int k = 0; k < ((int)strlen(cwd)) - 1; k++) {
-					if (cwd[k] == '/') {
-						slash_index = k;
-					}
-				}
-				char* newPath = malloc(32 * sizeof(char));
-				newPath = strncpy(newPath, cwd, slash_index);
-				chdir(newPath);
-			}
-			else {
-				if (chdir(path) < 0) {
-					fprintf(stderr, "Error: no such directory\n");
-					continue;
-				}
+		else if (!strcmp(cmd, "pwd")) {
+			retval = CALL_PWD(retval);
+			fprintf(stdout, "+ completed '%s' [%d]\n", cmd, retval);
+		}
+		else if (!strcmp(cmd, "cd")) {
+			retval = CALL_CD(Run.processes[0].args[1], retval);
+			fprintf(stdout, "+ completed '%s' [%d]\n", cmd, retval);
+		}
+		else if (!strcmp(cmd, "pushd")) {
+			char buf[CMDLINE_MAX];
+			getcwd(buf, CMDLINE_MAX);
+			PushStack(buf);
+			fprintf(stdout, "+ completed '%s' [0]\n", cmd);
+		}
+		else if (!strcmp(cmd, "popd")) {
+			PopStack();
+			fprintf(stdout, "+ completed '%s' [0]\n", cmd);
+		}
+		else if (!strcmp(cmd, "dirs")) {
+			display();
+			fprintf(stdout, "+ completed '%s' [0]\n", cmd);
+		}
+		else {
+			retval = PIPE_CMD(&Run);
+			if (retval != 0) {
+				fprintf(stderr, "Error: command not found\n");
 			}
 			fprintf(stdout, "+ completed '%s' [%d]\n", cmd, retval);
-			continue;
 		}
-
-
-		/* Regular command */
-		retval = SYS_CALL(&Run);
-		if (retval != 0) {
-			fprintf(stderr, "Error: command not found\n");
-		}
-		fprintf(stdout, "+ completed '%s' [%d]\n", cmd, retval);
 	}
-
 	return EXIT_SUCCESS;
 }
